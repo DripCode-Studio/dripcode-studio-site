212ad935d7d7b17c597ecf37675d682a4c2f99c7
Ismael Loko <lokoismael30@gmail.com>
2025-07-09 22:20:55 -0400

    implementing language and them context

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "../contexts/ThemeContext.jsx";
+import { LanguageProvider } from "../contexts/LanguageContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
     </ThemeProvider>
   </StrictMode>
 );

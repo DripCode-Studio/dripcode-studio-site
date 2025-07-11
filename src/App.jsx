7065eeabeab334a17bd834ceee4607b3d095c0a9
Ismael Loko <lokoismael30@gmail.com>
2025-07-10 21:02:40 -0400

    loading dotenv on App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Portfolio from "./components/Portfolio";
 import Services from "./components/Services";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
+import dotenv from "dotenv";
+dotenv.config();
+
 function App() {
   return (
     <>
